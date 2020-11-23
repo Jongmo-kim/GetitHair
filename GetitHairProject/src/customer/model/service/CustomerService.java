a2ded58e31246fb998aeecb29b75f666016ddc3b
taemin.jeon <wjsxoals1992@naver.com>
2020-11-23 18:40:23 +0900

    손님마이페이지, 조회,수정,탈퇴
    customer.model부분 수정
    MypageCustSerlvet추가
    UpdateCustomerSerlvet 추가
    DeleteCustometSerlvet 추가
    WEB-INF/views/customer 폴더 추가

--- GetitHairProject/src/customer/model/service/CustomerService.java
+++ GetitHairProject/src/customer/model/service/CustomerService.java
@@ -1,5 +1,51 @@
 package customer.model.service;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import common.JDBCTemplate;
+import customer.model.dao.CustomerDao;
+import customer.model.vo.Customer;
+
+
 public class CustomerService {
 
+	public Customer selectOneCustomer(String customerId) {
+		Connection conn = JDBCTemplate.getConnection();
+		Customer loginCustomer = new CustomerDao().selectOneCustomer(conn,customerId);
+		JDBCTemplate.close(conn);
+		return loginCustomer;
+	}
+	public ArrayList<Customer> selectAllMember() {
+		Connection conn = JDBCTemplate.getConnection();
+		ArrayList<Customer> list = new CustomerDao().selectAllMember(conn);
+		JDBCTemplate.close(conn);
+		return list;
+	}
+	public int updateCustomer(Customer customer) {
+		Connection conn = JDBCTemplate.getConnection();
+		int result = 0;
+		result = new CustomerDao().updateMember(conn,customer);
+		if(result>0) {
+			JDBCTemplate.commit(conn);
+		}else {
+			JDBCTemplate.rollback(conn);
+		}
+		JDBCTemplate.close(conn);
+		return result;
+	}
+	public int deleteCustomer(String customerId) {
+		Connection conn = JDBCTemplate.getConnection();
+		int result = 0;
+		result = new CustomerDao().deleteCustomer(conn,customerId);
+		if(result>0) {
+			JDBCTemplate.commit(conn);
+		}else {
+			JDBCTemplate.rollback(conn);
+		}
+		JDBCTemplate.close(conn);
+		return result;
+	}
+	
+	
 }
