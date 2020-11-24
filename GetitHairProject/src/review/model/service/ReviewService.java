3a6e389aaf6b4181c7c1101f2e134e0836bea2fe
ehgus8 <jaehaeee@naver.com>
2020-11-24 16:03:10 +0900

    add insertData.sql , update createTable.sql, update review dao,service

--- GetitHairProject/src/review/model/service/ReviewService.java
+++ GetitHairProject/src/review/model/service/ReviewService.java
@@ -8,13 +8,25 @@ import review.model.dao.ReviewDao;
 import review.model.vo.Review;
 
 public class ReviewService {
+	//INSERT 구문
+//	public int insertReview(Review rv) {
+//		Connection conn = JDBCTemplate.getConnection();
+//		int result = new ReviewDao().insertReview(conn,rv);
+//		if(result = 0) {
+//			
+//		}
+//	}
+	
+	
+	
+	
+	//SELECT 구문
 	public ArrayList<Review> selectAllReview(){
 		Connection conn = JDBCTemplate.getConnection();
 		ArrayList<Review> list = new ReviewDao().selectAllReview(conn);
 		JDBCTemplate.close(conn);
 		return list;
 	}
-	
 	public ArrayList<Review> selectAllReviewById(String keyword){
 		Connection conn = JDBCTemplate.getConnection();
 		int customerNo = new ReviewDao().selectCustomerNoById(conn,keyword);
