1c5f047e8d1891a468ac8da12fe53ef0970b407a
gleam0721 <gleam0721@naver.com>
2020-11-26 12:04:36 +0900

    service 진짜 수정...!

--- GetitHairProject/src/designer/model/service/DesignerService.java
+++ GetitHairProject/src/designer/model/service/DesignerService.java
@@ -7,7 +7,7 @@ import designer.model.dao.DesignerDao;
 import designer.model.vo.Designer;
 
 public class DesignerService {
-	public Designer selectOneMember(int designerNo) {
+	public Designer selectOneDesigner(int designerNo) {
 		Connection conn = JDBCTemplate.getConnection();
 		Designer result = new DesignerDao().selectOneDesigner(conn, designerNo);
 		JDBCTemplate.close(conn);
