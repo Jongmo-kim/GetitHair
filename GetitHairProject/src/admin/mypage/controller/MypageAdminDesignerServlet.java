9e49d4a063f455047f0d497b632f67bf137cb4bb
ehgus8 <jaehaeee@naver.com>
2020-12-02 12:10:29 +0900

    update adminPage,update designerService,Dao

--- GetitHairProject/src/admin/mypage/controller/MypageAdminDesignerServlet.java
+++ GetitHairProject/src/admin/mypage/controller/MypageAdminDesignerServlet.java
@@ -37,30 +37,37 @@ public class MypageAdminDesignerServlet extends HttpServlet {
 		request.setCharacterEncoding("utf-8");
 		// 전달 받은 값 저장
 		// type value 1 : 아이디로 검색 , 2 : 이름으로 검색
-		int type = request.getParameter("searchType") != null ? Integer.parseInt(request.getParameter("searchType"))
+		int type = request.getParameter("searchType") != null && request.getParameter("searchType") != ""
+				? Integer.parseInt(request.getParameter("searchType"))
 				: 0;
 		String keyword = request.getParameter("keyword");
 		// reqPage = 요청할 페이지 번호.
 		int reqPage = request.getParameter("reqPage") != null ? Integer.parseInt(request.getParameter("reqPage")) : 1;
 		// 한 페이지에 출력될 리스트 최대 갯수.
-		int maxPrintSize = request.getParameter("list_num") != null ? Integer.parseInt(request.getParameter("list_num")): 20;
+		int maxPrintSize = request.getParameter("list_num") != null ? Integer.parseInt(request.getParameter("list_num"))
+				: 20;
 		// 비즈니스 로직
-		int pageSize = 0;
-		ArrayList<Designer> list = null;
+		int maxPageSize = 0;
+		Object[] objList = null;
+		ArrayList<Designer> list;
 		list = new DesignerService().selectAllDesigner();
 		if (type == 1 && !keyword.equals("")) { // 검색으로 얻어올 경우
-//			list = new DesignerService().selectAllDesignerById(keyword,reqPage,maxPrintSize);
+			objList = new DesignerService().selectAllDesignerById(keyword, reqPage, maxPrintSize);
+			maxPageSize = (Integer) objList[0];
+			list = (ArrayList<Designer>) objList[1];
 		} else if (type == 2 && !keyword.equals("")) { // name 검색일 경우
-
+			objList = new DesignerService().selectAllDesignerByName(keyword, reqPage, maxPrintSize);
+			maxPageSize = (Integer) objList[0];
+			list = (ArrayList<Designer>) objList[1];
 		} else { // 검색이 아닐때 모든 디자이너를 반환.
-			pageSize = new DesignerService().getAllDesignerMaxPageSize(maxPrintSize);
+			maxPageSize = new DesignerService().getAllDesignerMaxPageSize(maxPrintSize);
 			list = new DesignerService().selectAllDesigner(reqPage, maxPrintSize);
 		}
 		int maxSize = 5;
-		int[] startEnd = PagingCommon.getPageStartEnd(reqPage, maxSize, pageSize);
+		int[] startEnd = PagingCommon.getPageStartEnd(reqPage, maxSize, maxPageSize);
 		// 값 전달
 		request.setAttribute("list", list);
-		request.setAttribute("pageSize", pageSize);
+		request.setAttribute("pageSize", maxPageSize);
 		request.setAttribute("keyword", keyword);
 		request.setAttribute("pageStart", startEnd[0]);
 		request.setAttribute("pageEnd", startEnd[1]);
--- GetitHairProject/src/admin/mypage/model/dao/AdminDao.java
+++ GetitHairProject/src/admin/mypage/model/dao/AdminDao.java
@@ -39,61 +39,26 @@ public class AdminDao {
 		
 		return admin;
 	}
-	public ArrayList<Customer> getCustomerListById(Connection conn, String id) {
+	public Object[] getCustomerListById(Connection conn, String keyword, int reqPage, int maxPrintSize) {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		String qrySelect = "select * from customer where customer_id like ?";
+		String qrySelect = "SELECT R.*,(SELECT COUNT(*) FROM (SELECT D.* FROM customer D WHERE customer_id LIKE ? ORDER BY D.customer_NO DESC) D) as cnt FROM (SELECT ROWNUM RN,D.* FROM "
+				+ "(SELECT D.* FROM customer D WHERE customer_id LIKE ? ORDER BY D.customer_NO DESC) D) R "
+				+ "WHERE RN BETWEEN ? AND ?";
 		ArrayList<Customer> list = null;
-		
-		try {
-			pstmt = conn.prepareStatement(qrySelect);
-			pstmt.setString(1, id+"%");
-			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
-				if(list == null)
-					list = new ArrayList<Customer>();
-				
-				Customer cust = new Customer();
-				cust.setAddrDetail(rs.getString("addr_detail"));
-				cust.setAddrPostcode(rs.getString("addr_postcode"));
-				cust.setCustomerAddr(rs.getString("customer_addr"));
-				cust.setCustomerEmail(rs.getString("customer_email"));
-				cust.setCustomerGen(rs.getString("customer_gen"));
-				cust.setCustomerId(rs.getString("customer_id"));
-				cust.setCustomerName(rs.getString("customer_name"));
-				cust.setCustomerNo(rs.getInt("customer_no"));
-				cust.setCustomerPhone(rs.getString("customer_phone"));
-				cust.setCustomerPw(rs.getString("customer_pw"));
-				cust.setCustomerBirthdate(rs.getDate("customer_birthdate"));
-				cust.setCustomerEnrolldate(rs.getDate("customer_enrolldate"));
-				list.add(cust);
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
-			JDBCTemplate.close(rs);
-			JDBCTemplate.close(pstmt);
-		}
-		return list;
-	}
-
-	public ArrayList<Customer> getCustomerListByName(Connection conn, String keyword) {
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String qrySelect = "select * from customer where customer_name like ?";
-		ArrayList<Customer> list = null;
-		
+		int cnt =0;
 		try {
 			pstmt = conn.prepareStatement(qrySelect);
 			pstmt.setString(1, keyword+"%");
+			pstmt.setString(2, keyword+"%");
+			pstmt.setInt(3, (maxPrintSize*(reqPage-1))+1);
+			pstmt.setInt(4, maxPrintSize*reqPage);
 			rs = pstmt.executeQuery();
 			
 			while(rs.next()) {
 				if(list == null)
 					list = new ArrayList<Customer>();
-				
+				cnt = rs.getInt("cnt");
 				Customer cust = new Customer();
 				cust.setAddrDetail(rs.getString("addr_detail"));
 				cust.setAddrPostcode(rs.getString("addr_postcode"));
@@ -116,6 +81,51 @@ public class AdminDao {
 			JDBCTemplate.close(rs);
 			JDBCTemplate.close(pstmt);
 		}
-		return list;
+		return new Object[] {cnt,list};
+	}
+
+	public Object[] getCustomerListByName(Connection conn, String keyword, int reqPage, int maxPrintSize) {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String qrySelect = "SELECT R.*,(SELECT COUNT(*) FROM (SELECT D.* FROM customer D WHERE customer_name LIKE ? ORDER BY D.customer_NO DESC) D) as cnt FROM (SELECT ROWNUM RN,D.* FROM "
+				+ "(SELECT D.* FROM customer D WHERE customer_name LIKE ? ORDER BY D.customer_NO DESC) D) R "
+				+ "WHERE RN BETWEEN ? AND ?";
+		ArrayList<Customer> list = null;
+		int cnt =0;
+		try {
+			pstmt = conn.prepareStatement(qrySelect);
+			pstmt.setString(1, keyword+"%");
+			pstmt.setString(2, keyword+"%");
+			pstmt.setInt(3, (maxPrintSize*(reqPage-1))+1);
+			pstmt.setInt(4, maxPrintSize*reqPage);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				if(list == null)
+					list = new ArrayList<Customer>();
+				cnt = rs.getInt("cnt");
+				Customer cust = new Customer();
+				cust.setAddrDetail(rs.getString("addr_detail"));
+				cust.setAddrPostcode(rs.getString("addr_postcode"));
+				cust.setCustomerAddr(rs.getString("customer_addr"));
+				cust.setCustomerEmail(rs.getString("customer_email"));
+				cust.setCustomerGen(rs.getString("customer_gen"));
+				cust.setCustomerId(rs.getString("customer_id"));
+				cust.setCustomerName(rs.getString("customer_name"));
+				cust.setCustomerNo(rs.getInt("customer_no"));
+				cust.setCustomerPhone(rs.getString("customer_phone"));
+				cust.setCustomerPw(rs.getString("customer_pw"));
+				cust.setCustomerBirthdate(rs.getDate("customer_birthdate"));
+				cust.setCustomerEnrolldate(rs.getDate("customer_enrolldate"));
+				list.add(cust);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JDBCTemplate.close(rs);
+			JDBCTemplate.close(pstmt);
+		}
+		return new Object[] {cnt,list};
 	}
 }
