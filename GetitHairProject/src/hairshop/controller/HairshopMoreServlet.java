eb9c7f7bf908fef0fef94bdf0f176d3671b98c8c
송히 <dlqnslthdgml@naver.com>
2020-12-02 16:34:36 +0900

    이미지 db 수정

--- GetitHairProject/src/hairshop/controller/HairshopMoreServlet.java
+++ GetitHairProject/src/hairshop/controller/HairshopMoreServlet.java
@@ -37,11 +37,10 @@ public class HairshopMoreServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		int start = Integer.parseInt(request.getParameter("start"));
-		ArrayList<ImageList> shopimg = new ImageService().selectAllImageListByType("hairshop");
+		ArrayList<ImageList> shopimg = new ImageService().selectAllImageListByType("hairshop");//넘버로
 		ArrayList<Hairshop> list = new HairshopService().hairshopMore(start);
 		response.setCharacterEncoding("utf-8");
-		new Gson().toJson(list,response.getWriter());
-		new Gson().toJson(shopimg,response.getWriter());
+		new Gson().toJson(list,response.getWriter());//객체 새로 만들어서 묶어서 보내기
 	}
 
 	/**
