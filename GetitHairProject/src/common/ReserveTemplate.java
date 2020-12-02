3007feae96057e1e53a4f2c603bd8bd3550cdf15
taemin.jeon <wjsxoals1992@naver.com>
2020-12-02 13:55:44 +0900

    다시예약하기 구현완료 시간+날짜 같이 받아짐 ㅋ(종모의도움의손이컷음)

--- GetitHairProject/src/common/ReserveTemplate.java
+++ GetitHairProject/src/common/ReserveTemplate.java
@@ -14,6 +14,8 @@ import designer.model.vo.Designer;
 import hairshop.model.service.HairshopService;
 import hairshop.model.vo.Hairshop;
 import reserve.model.vo.Reserve;
+import stylelist.model.service.StylelistService;
+import stylelist.model.vo.Stylelist;
 
 public class ReserveTemplate {
 	public static Reserve setReserve(HttpServletRequest request) {
@@ -21,11 +23,13 @@ public class ReserveTemplate {
 		int customerNo = request.getParameter("customerNo")==null ?  -1:Integer.parseInt(request.getParameter("customerNo"));
 		int designerNo = request.getParameter("designerNo")==null ?  -1:Integer.parseInt(request.getParameter("designerNo"));
 		int shopNo = request.getParameter("shopNo")==null ?  -1:Integer.parseInt(request.getParameter("shopNo"));
-		String reserveDate = request.getParameter("reserveDate");
+		int stylelistNo = request.getParameter("stylelistNo")==null ?  -1:Integer.parseInt(request.getParameter("stylelistNo"));
 		String reserveStatus = request.getParameter("reserveStatus");
 		String reserveCustReq = request.getParameter("reserveCustReq");
 		String reserveDesignerReq = request.getParameter("reserveDesignerReq");
 		String reserveDesignerMemo = request.getParameter("reserveDesignerMemo");
+		String reserveDate = request.getParameter("reserveDate");
+		//reserveDate //start End 구현 못함 시간부족으로 확인 필요 yyyy-MM-dd
 		Customer c = new CustomerService().selectOneCustomer(customerNo);
 		if(c==null) {
 			c = new Customer();
@@ -38,9 +42,15 @@ public class ReserveTemplate {
 		if( s ==null) {
 			s = new Hairshop();
 		}
+		Stylelist sl = new StylelistService().selectOneStylelist(stylelistNo);
+		if( sl == null) {
+			sl = new Stylelist();
+		}
 		Reserve r = new Reserve();
 		r.setCustomer(c);
 		r.setDesigner(d);
+		r.setShop(s);
+		r.setStylelist(sl);
 		r.setReserveCustReq(reserveCustReq);
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
