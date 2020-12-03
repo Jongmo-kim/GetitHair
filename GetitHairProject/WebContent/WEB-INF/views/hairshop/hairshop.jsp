7ebae191633762f22fbd200812558a511322c10f
송히 <dlqnslthdgml@naver.com>
2020-12-03 23:20:45 +0900

    last

--- GetitHairProject/WebContent/WEB-INF/views/hairshop/hairshop.jsp
+++ GetitHairProject/WebContent/WEB-INF/views/hairshop/hairshop.jsp
@@ -6,6 +6,7 @@
     <%
     	ArrayList<Hairshop> list = (ArrayList<Hairshop>)request.getAttribute("list");
         int totalCount = (Integer)request.getAttribute("totalCount");
+        ArrayList<String> filepath = (ArrayList<String>)request.getAttribute("filepath");
     %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
--- GetitHairProject/src/hairshop/controller/HairshopServlet.java
+++ GetitHairProject/src/hairshop/controller/HairshopServlet.java
@@ -37,9 +37,19 @@ public class HairshopServlet extends HttpServlet {
 		request.setCharacterEncoding("utf-8");
 		ArrayList<Hairshop> list = new HairshopService().selectHairshop();
 		int totalCount = new HairshopService().totalCount();
+		ArrayList<String> filepath = new ArrayList<String>();
+		String filepath1 = new ImageService().selectFilepath("style", 1);
+		String filepath2 = new ImageService().selectFilepath("style", 2);
+		String filepath3 = new ImageService().selectFilepath("style", 3);
+		String filepath4 = new ImageService().selectFilepath("style", 4);
+		filepath.add(filepath1);
+		filepath.add(filepath2);
+		filepath.add(filepath3);
+		filepath.add(filepath4);
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/hairshop/hairshop.jsp");
 		request.setAttribute("list", list);
 		request.setAttribute("totalCount", totalCount);
+		request.setAttribute("filepath", filepath);
 		rd.forward(request, response);
 	}
 
